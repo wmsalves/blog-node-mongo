ff9309808e64de4d5c8da66f4004b1c74f27d78c
wemer <wemersoncontas22@gmail.com>
2025-02-07 10:07:31 -0300

    feat: implement authentication with session, JWT, bcrypt and MongoDB store

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ require("dotenv").config();
 
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo');
 
 const connectDB = require('./server/config/db');
+const session = require("express-session");
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
@@ -12,6 +15,17 @@ connectDB();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(cookieParser)
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI
+  }),
+    //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+}));
 
 app.use(express.static("public"));
 
